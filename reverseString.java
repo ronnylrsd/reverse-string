9d0620a5fef219aa4a8d2089efc19894c86cca01
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2024-02-01 11:02:15 -0300

    Feat: add reversed string with loop

--- reverseString.java
+++ reverseString.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class reverseString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Escreva aljuma coisa: ");
+        System.out.println("Escreva alguma coisa: ");
         String normalString = scanner.nextLine();
-        String reverseString = functionReverseStringWithAPI(normalString);
+        String reverseString = functionReverseStringWithLoop(normalString);
         System.out.println("A string invertida é: " + reverseString);
         scanner.close();
     }
@@ -13,6 +13,14 @@ public class reverseString {
     private static String functionReverseStringWithAPI(String str) {
         var stringBuffer = new StringBuffer(str);
         return stringBuffer.reverse().toString();
+    }
+
+    private static String functionReverseStringWithLoop(String str) {
+        StringBuilder reversedString = new StringBuilder();
+        for(int i = str.length() - 1; i >= 0; i--) {
+            reversedString.append(str.charAt(i));
+        }
+        return reversedString.toString();
     }
 
 }
